fbb9f9cdd4405341980e5e6a618446bc1a70c25a
ishtiyaqe <129334637+ishtiyaqe@users.noreply.github.com>
2024-03-07 04:27:18 +0600

    Update asgi.py

--- aicctv/asgi.py
+++ aicctv/asgi.py
@@ -1,11 +1,14 @@
 # asgi.py
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aicctv.settings')
+import django
+django.setup()
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import modelTraining.routing  # Import your routing configuration
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aicctv.settings')
+
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),  # Default Django HTTP handling
