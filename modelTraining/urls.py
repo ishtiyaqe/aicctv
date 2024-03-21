32c061b58c6b7ac8cd9ab09736c1a8fe7eda5b3a
ishtiyaqe <129334637+ishtiyaqe@users.noreply.github.com>
2024-03-22 01:25:46 +0600

    Update urls.py

--- modelTraining/urls.py
+++ modelTraining/urls.py
@@ -8,11 +8,14 @@ from .views import *
 urlpatterns = [
     path('api/login/', UserLogin.as_view(), name='login'),
 	path('api/logout/', UserLogout.as_view(), name='logout'),
+	path('verify_otp/', VerifyOTP.as_view(), name='verify_otp'),
 	path('api/user/', UserView.as_view(), name='user'),
     path('api/user_profiles/', UserProfileViews.as_view(), name='user-profiles'),
+    path('api/user_profiles_email/', UserProfileEmailViews.as_view(), name='user-profiles-emial'),
     path('api/cameras/', CameraCreateView.as_view(), name='camera-create'),
     path('cameras/', CameraListView.as_view(), name='camera-list'),
     path('camera-warning-videos/<int:camera_id>/', CameraWarningVideoListView.as_view(), name='camera-warning-videos'),
+    path('camera-warning-label/<int:camera_id>/', CameraWarningLabelListListView.as_view(), name='camera-warning-label'),
     path('warning-images/', CameraWarningVideoLists.as_view(), name='warning-images'),
 ]
 
